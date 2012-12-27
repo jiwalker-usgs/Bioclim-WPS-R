2329ad98ca1d6281fe45743a6734203afb735aba
dblodgett-usgs <dblodgett@usgs.gov>
2012-12-27 10:55:53 -0600

    commented out input variables to script will work properly with Rserve

---
+++ pyGDP_bioclim_prism_multi_client.py
@@ -0,0 +1,14 @@
+# This example will execute the bioclim_prism script deployed on a local host wps server.
+import pyGDP
+# Set this to remote server once the algorithm is published.
+pyGDP.WPS_URL='http://localhost:8080/wps/WebProcessingService'
+testpyGDP=pyGDP.pyGDPwebProcessing()
+processid='org.n52.wps.server.r.bioclim_prism_multi_latlon'
+inputs = [("start",'1950-01-01'), 
+  ("end", '2000-01-01'), 
+  ("lat_in", '43.00, 42.00'), 
+  ("lon_in",'-90.00, -91.00'),
+  ("ids", 'a, b')]
+output = "output"
+verbose=True
+filehandle=testpyGDP._executeRequest(processid, inputs, output, verbose)
