994634abe550f4c7662cac5a5b1b5ed65f246437
dblodgett-usgs <dblodgett@usgs.gov>
2013-03-29 14:12:00 -0500

    lots of debugging. it now sorts by run.

--- bcca_aggregation/bcca_aggregation.py
+++ bcca_aggregation/bcca_aggregation.py
@@ -1,10 +1,10 @@
 f=open('./bcca.ls')
 lines=f.readlines()
 f.close()
-daily_ncml_1='<?xml version="1.0"?><netcdf xmlns="http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2"><aggregation dimName="time" type="joinExisting"><scan location='
-daily_ncml_2=' regExp=".*monthly.*.nc" subdirs="false"/></aggregation></netcdf>'
-monthly_ncml_1='<?xml version="1.0"?><netcdf xmlns="http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2"><aggregation dimName="time" type="joinExisting"><scan location='
-monthly_ncml_2=' regExp=".*monthly.*.nc" subdirs="false"/></aggregation></netcdf>'
+daily_ncml_1='<?xml version="1.0"?>\n<netcdf xmlns="http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2">\n <aggregation dimName="time" type="joinExisting">\n  <scan location='
+daily_ncml_2=' regExp=".*(?&lt;<!monthly)\.\d{4}.nc" subdirs="false"/>\n </aggregation>\n</netcdf>'
+monthly_ncml_1='<?xml version="1.0"?>\n<netcdf xmlns="http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2">\n <aggregation dimName="time" type="joinExisting">\n  <scan location='
+monthly_ncml_2=' regExp=".*monthly.*.nc" subdirs="false"/>\n </aggregation>\n</netcdf>'
 for line in lines: # Loop through all lines, build unions and joinexistings as we go through.
 	if line[0]=='/': #if line is a path, do some union stuff if the path is terminal.
 		path=line[0:-1-2] # Paths end in :\n so take off last two characters.
@@ -22,9 +22,10 @@ for line in lines: # Loop through all lines, build unions and joinexistings as w
 			f=open('./ncmls/joins/'+name_monthly,'w')
 			f.write(monthly_ncml_1+'"'+path+'"'+monthly_ncml_2)
 			f.close()
+
 # 3 grids, 2 time resolutions, 5 calendars, 3 time periods.
-grids=('bc','bcca','regrid')
-calendars=('365_day','gregorian','360_day','noleap','OBS')
+grids=('/bc','/bcca','/regrid')
+calendars=('365_day','gregorian','360_day','noleap','obs')
 time_prds=('20c3m','sresa1b','sresa2','sresb1')
 # two time resolutions for all, no search needed.
 for grid in grids:
@@ -42,23 +43,24 @@ for grid in grids:
 						if open_switch==0:
 							d=open('./ncmls/unions/'+grid+'.'+calendar+'.'+time_prd+'','w')
 							m=open('./ncmls/unions/'+grid+'.'+calendar+'.'+time_prd+'.monthly','w')
-							d.write('<?xml version="1.0" encoding="UTF-8"?><netcdf xmlns="http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2"><aggregation type="union">')
-							m.write('<?xml version="1.0" encoding="UTF-8"?><netcdf xmlns="http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2"><aggregation type="union">')
+							d.write('<?xml version="1.0" encoding="UTF-8"?>\n<netcdf xmlns="http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2">\n <aggregation type="union">\n')
+							m.write('<?xml version="1.0" encoding="UTF-8"?>\n<netcdf xmlns="http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2">\n <aggregation type="union">\n')
 							open_switch=1
-						if grid and calendar and time_prd in path:
+						if grid in path[30:-1] and calendar in path and time_prd in path:
+							print path
 							name=line[0:-1-8]+'ncml' # create name of join file.
 							name_monthly=line[0:-1-8]+'monthly.ncml' # Create name for monthly join file.
-							var_name=line[0:-1-9] # create unique name for variable.
-							var_name_monthly=line[0:-1-8]+'monthly' # Create unique name for monthly variables.
+							var_name=line[0:-1-9].replace('.','-') # create unique name for variable.
+							var_name_monthly=line[0:-1-8].replace('.','-')+'monthly' # Create unique name for monthly variables.
 							if 'pr' in path:
 								orgName='pr'
 							if 'tasmax' in path:
 								orgName='tasmax'
 							if 'tasmin' in path:
 								orgName='tasmin'
-							d.write('<netcdf location="'+name+'"><variable orgName="'+orgName+'" name="'+var_name+'" /></netcdf>')
-							m.write('<netcdf location="'+name_monthly+'"><variable orgName="'+orgName+'" name="'+var_name_monthly+'" /></netcdf>')
-			d.write('</aggregation></netcdf>')
-			m.write('</aggregation></netcdf>')
+							d.write('  <netcdf location="../joins/'+name+'">\n   <variable orgName="'+orgName+'" name="'+var_name+'" />\n  </netcdf>\n')
+							m.write('  <netcdf location="../joins/'+name_monthly+'">\n   <variable orgName="'+orgName+'" name="'+var_name_monthly+'" />\n  </netcdf>\n')
+			d.write(' </aggregation>\n</netcdf>')
+			m.write(' </aggregation>\n</netcdf>')
 			d.close()
 			m.close()
