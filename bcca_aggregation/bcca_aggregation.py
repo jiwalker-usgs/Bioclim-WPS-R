f8b4b811540d4fb5f485385841cf62b89063fac7
dblodgett-usgs <dblodgett@usgs.gov>
2013-03-29 14:14:45 -0500

    missed some stuff in the last commit

--- bcca_aggregation/bcca_aggregation.py
+++ bcca_aggregation/bcca_aggregation.py
@@ -26,40 +26,43 @@ for line in lines: # Loop through all lines, build unions and joinexistings as w
 # 3 grids, 2 time resolutions, 5 calendars, 3 time periods.
 grids=('/bc','/bcca','/regrid')
 calendars=('365_day','gregorian','360_day','noleap','obs')
-time_prds=('20c3m','sresa1b','sresa2','sresb1')
+time_prds=('20c3m','sres')
+runs=('init','run1','run2','run3')
 # two time resolutions for all, no search needed.
 for grid in grids:
 	for calendar in calendars:
 		for time_prd in time_prds:
-			open_switch=0
-			for line in lines: # This loops through everything a lot of times and is slow, but it doesn't take long and it was easy to write this way. Probably a better way, but it does the job.
-				if line[0]=='/': #if line is a path, do some union stuff if the path is terminal.
-					path=line[0:-1-2] # Paths end in :\n so take off last two characters.
-					path_switch=1 # Set the "path switch". If its on, and the next line is a file, will write a union.
-				elif '.nc' in line: 
-					if path_switch==1: # If the last line was a path, write two join files, monthly and daily.
-						path_switch=0 # Turn off path switch.
-						# open and write start of union files
-						if open_switch==0:
-							d=open('./ncmls/unions/'+grid+'.'+calendar+'.'+time_prd+'','w')
-							m=open('./ncmls/unions/'+grid+'.'+calendar+'.'+time_prd+'.monthly','w')
-							d.write('<?xml version="1.0" encoding="UTF-8"?>\n<netcdf xmlns="http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2">\n <aggregation type="union">\n')
-							m.write('<?xml version="1.0" encoding="UTF-8"?>\n<netcdf xmlns="http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2">\n <aggregation type="union">\n')
-							open_switch=1
-						if grid in path[30:-1] and calendar in path and time_prd in path:
-							print path
-							name=line[0:-1-8]+'ncml' # create name of join file.
-							name_monthly=line[0:-1-8]+'monthly.ncml' # Create name for monthly join file.
-							var_name=line[0:-1-9].replace('.','-') # create unique name for variable.
-							var_name_monthly=line[0:-1-8].replace('.','-')+'monthly' # Create unique name for monthly variables.
-							if 'pr' in path:
-								orgName='pr'
-							if 'tasmax' in path:
-								orgName='tasmax'
-							if 'tasmin' in path:
-								orgName='tasmin'
-							d.write('  <netcdf location="../joins/'+name+'">\n   <variable orgName="'+orgName+'" name="'+var_name+'" />\n  </netcdf>\n')
-							m.write('  <netcdf location="../joins/'+name_monthly+'">\n   <variable orgName="'+orgName+'" name="'+var_name_monthly+'" />\n  </netcdf>\n')
+			for run in runs:
+				open_switch=0
+				for line in lines: # This loops through everything a lot of times and is slow, but it doesn't take long and it was easy to write this way. Probably a better way, but it does the job.
+					if line[0]=='/': #if line is a path, do some union stuff if the path is terminal.
+						path=line[0:-1-2] # Paths end in :\n so take off last two characters.
+						path_switch=1 # Set the "path switch". If its on, and the next line is a file, will write a union.
+					elif '.nc' in line: 
+						if path_switch==1: # If the last line was a path, write two join files, monthly and daily.
+							path_switch=0 # Turn off path switch.
+							# open and write start of union files
+							if open_switch==0:
+								if 'init' in run:
+									d=open('./ncmls/unions/'+grid+'.'+calendar+'.'+time_prd+'','a')
+									m=open('./ncmls/unions/'+grid+'.'+calendar+'.'+time_prd+'.monthly','a')
+									d.write('<?xml version="1.0" encoding="UTF-8"?>\n<netcdf xmlns="http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2">\n <aggregation type="union">\n')
+									m.write('<?xml version="1.0" encoding="UTF-8"?>\n<netcdf xmlns="http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2">\n <aggregation type="union">\n')
+								open_switch=1
+							if grid in path[30:-1] and calendar in path and time_prd in path and run in path:
+								print path
+								name=line[0:-1-8]+'ncml' # create name of join file.
+								name_monthly=line[0:-1-8]+'monthly.ncml' # Create name for monthly join file.
+								var_name=line[0:-1-9].replace('.','-') # create unique name for variable.
+								var_name_monthly=line[0:-1-8].replace('.','-')+'monthly' # Create unique name for monthly variables.
+								if 'pr' in path:
+									orgName='pr'
+								if 'tasmax' in path:
+									orgName='tasmax'
+								if 'tasmin' in path:
+									orgName='tasmin'
+								d.write('  <netcdf location="../joins/'+name+'">\n   <variable orgName="'+orgName+'" name="'+var_name+'" />\n  </netcdf>\n')
+								m.write('  <netcdf location="../joins/'+name_monthly+'">\n   <variable orgName="'+orgName+'" name="'+var_name_monthly+'" />\n  </netcdf>\n')
 			d.write(' </aggregation>\n</netcdf>')
 			m.write(' </aggregation>\n</netcdf>')
 			d.close()
