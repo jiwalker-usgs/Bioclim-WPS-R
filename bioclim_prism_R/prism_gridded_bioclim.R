51ae881248ff46b8b2486a87a2df3f73ebe65aba
David Blodgett <dblodgett@usgs.gov>
2013-06-29 20:37:26 -0500

    Solved an issue with single year requests and got things to match the example provided by Colin Talbert.

--- bioclim_prism_R/prism_gridded_bioclim.R
+++ bioclim_prism_R/prism_gridded_bioclim.R
@@ -1,19 +1,20 @@
 # Define Inputs (will come from external call)
-start <- "1950-01-01"
-end <- "1960-12-31"
-bbox<-c(-91,41,-88,44)
+start <- "1950"
+end <- "1951"
+bbox<-c(-88,41,-84,44)
 OPeNDAP_URI<-"http://cida.usgs.gov/thredds/dodsC/prism"
 tmax_var  <- "tmx"
 tmin_var <- "tmn"
 prcp_var <- "ppt"
 tave_var <- "NULL"
-bioclims<-c(1,2,3)
+bioclims<-c(1)
 
 library("ncdf4")
 library("climates")
 library("rgdal")
 library("stats")
 library("utils")
+library("chron")
 dods_data <- nc_open(OPeNDAP_URI)
 # Get time index time origin.
 time_units<-strsplit(dods_data$dim$time$units, " ")[[1]]
@@ -22,11 +23,14 @@ date_origin<-time_units[3]
 time_origin<-"00:00:00"
 if(length(time_units)==4) time_origin<-time_units[4]
 cal_origin <- paste(date_origin, time_origin)
-t_1 <- julian(strptime(start, '%Y-%m-%d'), origin<-strptime(cal_origin, '%Y-%m-%d %H:%M:%S'))
-t_2 <- julian(strptime(end, '%Y-%m-%d'), origin<-strptime(cal_origin, '%Y-%m-%d %H:%M:%S'))
+year_origin=as.numeric(strsplit(date_origin,'-')[[1]][1])
+month_origin=as.numeric(strsplit(date_origin,'-')[[1]][2])
+day_origin=as.numeric(strsplit(date_origin,'-')[[1]][3])
+t_1 <- julian(strptime(paste(start,'-01-01',sep=''), '%Y-%m-%d'), origin<-strptime(cal_origin, '%Y-%m-%d %H:%M:%S'))
+t_2 <- julian(strptime(paste(end, '-01-01', sep='') '%Y-%m-%d'), origin<-strptime(cal_origin, '%Y-%m-%d %H:%M:%S'))
 # Some simple time and bbox validation.
-if (t_1<head(dods_data$dim$time$vals,1)) stop(paste("Submitted start date,",start, "is before the dataset's start date,",date_origin))
-if (t_2>tail(dods_data$dim$time$vals,1)) stop(paste("Submitted end date,",end, "is after the dataset's start date,",date_origin))
+if (t_1<head(dods_data$dim$time$vals,1)) stop(paste("Submitted start date,",start, "is before the dataset's start date,",chron(head(dods_data$dim$time$vals,1),out.format=c(dates="year-m-day"), origin=c(month=month_origin, day=day_origin, year=year_origin))))
+if (t_2>tail(dods_data$dim$time$vals,1)) stop(paste("Submitted end date,",end, "is after the dataset's end date,",chron(tail(dods_data$dim$time$vals,1),out.format=c(dates="year-m-day"), origin=c(month=month_origin, day=day_origin, year=year_origin))))
 if (t_1>t_2) stop('Start date must be before end date.')
 if (bbox[1]<min(dods_data$dim$lon$vals)) stop(paste("Submitted minimum longitude",bbox[1], "is outside the dataset's minimum",min(dods_data$dim$lon$vals)))
 if (bbox[2]<min(dods_data$dim$lat$vals)) stop(paste("Submitted minimum latitude",bbox[2], "is outside the dataset's minimum",min(dods_data$dim$lat$vals)))
@@ -53,7 +57,8 @@ tmin_data <- ncvar_get(dods_data, tmin_var, c(min(lon1_index,lon2_index),min(lat
 prcp_data <- ncvar_get(dods_data, prcp_var, c(min(lon1_index,lon2_index),min(lat1_index,lat2_index),t_ind1),c((abs(lon1_index-lon2_index)+1),(abs(lat1_index-lat2_index)+1),(t_ind2-t_ind1)))
 if (tave_var!="NULL") tave_data <- ncvar_get(dods_data, tave_var, c(min(lon1_index,lon2_index),min(lat1_index,lat2_index),t_ind1),c((abs(lon1_index-lon2_index)+1),(abs(lat1_index-lat2_index)+1),(t_ind2-t_ind1))) else tave_data <- (tmax_data+tmin_data)/2
 # The loops below calculate bioclim for each time series and assemble a matrix 'out_data' that is lon*lat X time steps X bioclim stats.
-out_data <- array(dim=c(nrow(tmax_data)*ncol(tmax_data),dim(tmax_data)[3]/12,length(bioclims)))
+years=dim(tmax_data)[3]/12
+out_data <- array(dim=c(nrow(tmax_data)*ncol(tmax_data),years,length(bioclims)))
 ind<-1
 for (row in 1:nrow(tmax_data))
 {
@@ -63,7 +68,17 @@ for (row in 1:nrow(tmax_data))
     tmin <- matrix(tmin_data[row,col,],dim(tmin_data)[3]/12,12)
     prcp <- matrix(prcp_data[row,col,],dim(prcp_data)[3]/12,12)
     tave <- matrix(tave_data[row,col,],dim(prcp_data)[3]/12,12)
-    bioclim<-data.frame(bioclim(tmin=tmin, tmax=tmax, prec=prcp, tmean=tave, bioclims))
+    if (length(bioclims)==1) # bioclim returns a vector rather than a dataFrame if only asked for one output.
+    {
+      bioclim<-bioclim(tmin=tmin, tmax=tmax, prec=prcp, tmean=tave, bioclims)
+      dim(bioclim)<-c(years,1)
+      colnames(bioclim)<-paste('bioclim_',bioclims, sep='')
+      bioclim<-data.frame(bioclim)
+    }
+    else
+    {
+      bioclim<-data.frame(bioclim(tmin=tmin, tmax=tmax, prec=prcp, tmean=tave, bioclims))
+    }
     for (bclim in 1:length(bioclims))
     {
       for (t in 1:dim(tmax_data)[3]/12)
@@ -83,7 +98,7 @@ if (abs(dif_lats-dif_lons)>0.00001)
   stop('The data source appears to be an irregular grid, this datatype is not supported.')
 for (row in 1:length(lons)) {
   for (col in length(lats):1)
-  {coords[ind,1]<-lons[row]-dif_lons/2
+  {coords[ind,1]<-lons[row]+dif_lons/2
    coords[ind,2]<-lats[col]-dif_lats/2
    ind<-ind+1
   }
