b7ae312fa59a6404ab48a6fdc578d9d851255a6c
David Blodgett <dblodgett@usgs.gov>
2013-07-09 20:30:03 -0500

    Last save.

--- gridded_bioclim.R
+++ gridded_bioclim.R
@@ -39,30 +39,12 @@ library("zoo")
 dods_data <- nc_open(OPeNDAP_URI)
 # Need to check if specified inputs exist in specified dataset and throw errors acordingly. 
 # Get time index time origin.
-time_units<-strsplit(dods_data$dim$time$units, " ")[[1]]
-time_step<-time_units[1]
-date_origin<-time_units[3]
-time_origin<-"00:00:00"
-if(length(time_units)==4) time_origin<-time_units[4]
-cal_origin <- paste(date_origin, time_origin)
-year_origin=as.numeric(strsplit(date_origin,'-')[[1]][1])
-month_origin=as.numeric(strsplit(date_origin,'-')[[1]][2])
-day_origin=as.numeric(strsplit(date_origin,'-')[[1]][3])
-years=as.numeric(end)-as.numeric(start)
-t_1 <- julian(strptime(paste(start,'-01-01 12:00',sep=''), '%Y-%m-%d %H:%M'), origin<-strptime(cal_origin, '%Y-%m-%d %H:%M:%S'))
-t_2 <- julian(strptime(paste(end, '-01-01 00:00', sep=''), '%Y-%m-%d %H:%M'), origin<-strptime(cal_origin, '%Y-%m-%d %H:%M:%S'))
-# Some simple time and bbox validation.
-if (t_1<head(dods_data$dim$time$vals,1)) stop(paste("Submitted start date,",start, "is before the dataset's start date,",chron(head(dods_data$dim$time$vals,1),out.format=c(dates="year-m-day"), origin=c(month=month_origin, day=day_origin, year=year_origin))))
-if (t_2>tail(dods_data$dim$time$vals,1)) stop(paste("Submitted end date,",end, "is after the dataset's end date,",chron(tail(dods_data$dim$time$vals,1),out.format=c(dates="year-m-day"), origin=c(month=month_origin, day=day_origin, year=year_origin))))
-if (t_1>t_2) stop('Start date must be before end date.')
-request_indices<-request_bbox(dods_data,tmax_var,bbox_in)
-t_ind1 <- min(which(abs(dods_data$dim$time$vals-t_1)==min(abs(dods_data$dim$time$vals-t_1))))
-t_ind2 <- max(which(abs(dods_data$dim$time$vals-t_2)==min(abs(dods_data$dim$time$vals-t_2))))
+request_time_indices<-request_time_bounds(dods_data,start,end)
+request_bbox_indices<-request_bbox(dods_data,tmax_var,bbox_in)
 #Pull out data needed for calculations and writing geotiffs. 
 # A loop should be introduced here to the end of the script to only pull in one year of data at a time.
-lons<-dods_data$dim$lon$vals[lon1_index:lon2_index]
-lats<-dods_data$dim$lat$vals[lat1_index:lat2_index]
-time<-dods_data$dim$time$vals[t_ind1:t_ind2-1]
+
+
 # Check for regular grid.
 dif_lons = mean(diff(lons))
 dif_lats = mean(diff(lats))
