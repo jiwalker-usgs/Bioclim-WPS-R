134fa507a3694ffb954da868c838cfc35e53e879
Blodgett, David L <dblodgett@usgs.gov>
2014-04-01 08:27:23 -0500

    update to R script that writes out time coordinate variable.

--- flint_scripts/write_time_nc.R
+++ flint_scripts/write_time_nc.R
@@ -1,16 +1,21 @@
 library("ncdf")
 library("chron")
-nc_filename<-'times_monthly.nc'
+nc_filename<-'times_monthly_'
 originDate<-strptime("2006-04-01","%Y-%m-%d")
-endDate<-strptime("2100-01-01","%Y-%m-%d")
-times<-seq(from=originDate,to=endDate,by='months')
-times<-round(julian(times,origin=originDate))
-time_vec<-1:length(times)
-nc_time_dim <- dim.def.ncdf('time','',time_vec,create_dimvar=FALSE)
-nc_time_var <- var.def.ncdf('time','days since 2006-04-01', nc_time_dim, -999, prec='integer')
-nc_file<-create.ncdf(nc_filename,list(nc_time_var))
-var<-put.var.ncdf(nc_file,nc_time_var,times)
-close.ncdf(nc_file)
+inc_originDate<-originDate
+years<-2006:2100
+for (i in 1:length(years))
+{
+  endDate<-strptime(paste(years[i],"-12-01",sep=""),"%Y-%m-%d")
+  times<-seq(from=inc_originDate,to=endDate,by='months')
+  times<-round(julian(times,origin=originDate))
+  time_vec<-1:length(times)
+  nc_time_dim <- dim.def.ncdf('time','',time_vec,create_dimvar=FALSE)
+  nc_time_var <- var.def.ncdf('time','days since 2006-04-01', nc_time_dim, -999, prec='integer')
+  nc_file<-create.ncdf(paste(nc_filename,years[i],'.nc',sep=""),list(nc_time_var))
+  var<-put.var.ncdf(nc_file,nc_time_var,times)
+  close.ncdf(nc_file)
+}
 
 nc_filename<-'times_monthly_hst'
 originDate<-strptime("1895-10-01","%Y-%m-%d")
