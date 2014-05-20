b7578cd9564521268ed5ece648aeacdc7c853da9
Blodgett, David L <dblodgett@usgs.gov>
2014-05-20 11:25:49 -0700

    Got a first cut at all the daily derivatives working.

--- bioclim_wps_R/gridded_bioclim.R
+++ bioclim_wps_R/gridded_bioclim.R
@@ -87,6 +87,49 @@ get_dap_data<-function(ncdf4_handle,x1,y1,x2,y2,time,t_ind1,t_ind2,origin,tmax_v
   return(list(tmax_data=tmax_data,tmin_data=tmin_data,prcp_data=prcp_data,tave_data=tave_data))
 }
 
+dap_climdex<-function(start,end,bbox_in,bioclims,OPeNDAP_URI,tmax_var,tmin_var,tave_var,prcp_var)
+{
+  te<-init_dap_bclim(OPeNDAP_URI,tmax_var,tmin_var,prcp_var,bioclims)
+  ncdf4_handle<-te$ncdf4_handle; temp_unit_func<-te$temp_unit_func
+  
+  te2<-request_bbox(ncdf4_handle,tmax_var,bbox_in); x1<-te2$x1; y1<-te2$y1; x2<-te2$x2; 
+  y2<-te2$y2; coords_master<-te2$coords_master; prj<-te2$prj
+  
+  fileNames<-c()
+  for (year in as.numeric(start):(as.numeric(end)))
+  {
+    #Call for time indices for this year
+    te3<-request_time_bounds(ncdf4_handle,year,year+1); t_ind1 <- te3$t_ind1; t_ind2<-te3$t_ind2; 
+    time_indices<-te3$time; origin<-te3$origin; time_PCICt<-as.PCICt(te3$time_posix,cal="gregorian")
+    
+    #Get the dap data
+    te4<-get_dap_data(ncdf4_handle,x1,y1,x2,y2,time,t_ind1,t_ind2,origin,tmax_var,tmin_var,prcp_var,tave_var=NULL,temp_unit_func)
+    tmax_data<-te4$tmax_data; tmin_data<-te4$tmin_data; prcp_data<-te4$prcp_data; tave_data<-te4$tave_data
+    
+    #Recast data to work for climdex
+    tmax_data<-t(tmax_data)
+    tmin_data<-t(tmin_data)
+    prcp_data<-t(prcp_data)
+    tave_data<-t(tave_data)
+    date_factor=factor(rep(year,length(tmin_data[,1])))
+    days_tmax_abv=apply(tmax_data, 2, number.days.op.threshold, date.factor=date_factor, threshold=days_tmax_abv_thresh, op=">")
+    days_tmin_blw=apply(tmin_data, 2, number.days.op.threshold, date.factor=date_factor, threshold=days_tmin_blw_thresh, op="<")
+    days_prcp_abv=apply(prcp_data, 2, number.days.op.threshold, date.factor=date_factor, threshold=days_prcp_abv_thresh, op=">")
+    longest_run_tmax_abv=apply(tmax_data, 2, spell.length.max, date.factor=date_factor, threshold=longest_run_tmax_abv_thresh, op=">",spells.can.span.years=FALSE)
+    longest_run_prcp_blw=apply(prcp_data, 2, spell.length.max, date.factor=date_factor, threshold=longest_run_prcp_blw_thresh, op="<",spells.can.span.years=FALSE)
+    trunc_0 <- function(x) { x[x<0] <- 0; x }
+    heating_degree_days_fun <- function(tave, heating_degree_day_thresh=18.333) { degree_days_out <- sum(trunc_0(heating_degree_day_thresh-tave)); degree_days_out}
+    heating_degree_days<-apply(tave_data, 2, heating_degree_days_fun)
+    cooling_degree_days_fun <- function(tave, cooling_degree_day_thresh=18.333) { degree_days_out <- sum(trunc_0(tave-cooling_degree_day_thresh)); degree_days_out}
+    cooling_degree_days<-apply(tave_data, 2, cooling_degree_days_fun)
+    growing_degree_days_fun <- function(tave, growing_degree_day_thresh=10) { degree_days_out <- sum(trunc_0(tave-growing_degree_day_thresh)); degree_days_out}
+    growing_degree_days<-apply(tave_data, 2, growing_degree_days_fun)
+    growing_season_lngth<-apply(tave_data, 2, growing.season.length, date.factor=date_factor, dates=time_PCICt, northern.hemisphere=TRUE)
+    #Run Bioclim and write to geotiff.
+    #fileNames<-append(fileNames,bioclim2geotiff(tmax_data,tmin_data,prcp_data,tave_data,bioclims, coords_master, prj, year))
+  }
+}
+
 dap_bioclim<-function(start,end,bbox_in,bioclims,OPeNDAP_URI,tmax_var,tmin_var,tave_var,prcp_var)
 {
   te<-init_dap_bclim(OPeNDAP_URI,tmax_var,tmin_var,prcp_var,bioclims)
